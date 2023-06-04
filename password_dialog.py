d3b4aa3385e6d306716cfc36559490bc4637c8d0
Kortelange <kortejakob@gmail.com>
2023-06-04 11:39:11 +0200

    Add documentation

--- password_dialog.py
+++ password_dialog.py
@@ -2,8 +2,31 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
 class PasswordDialog(tk.simpledialog.Dialog):
+    """
+    A dialog box for entering and confirming a password.
+
+    This class extends tkinter.simpledialog.Dialog, overriding the body,
+    validate, and apply methods. The Dialog class automatically calls
+    validate when the user tries to close the dialog, and if validate
+    returns True, it calls apply and then closes the dialog.
+
+    Methods:
+    body(master): creates the dialog's interface.
+    validate(): checks if the entered passwords match.
+    apply(): stores the password for later retrieval.
+    """
     def body(self, master):
+        """
+        Create the dialog's interface.
+
+        Parameters:
+        master (tkinter.Tk): The parent window of the dialog.
+
+        Returns:
+        tkinter.Entry: The first password entry field.
+        """
         tk.Label(master, text="Enter a password:").grid(row=0)
         tk.Label(master, text="Confirm password:").grid(row=1)
 
@@ -16,6 +39,12 @@ class PasswordDialog(tk.simpledialog.Dialog):
         return self.password_entry1
 
     def validate(self):
+        """
+        Check if the entered passwords match.
+
+        Returns:
+        bool: True if the passwords match, False otherwise.
+        """
         password1 = self.password_entry1.get()
         password2 = self.password_entry2.get()
 
@@ -30,4 +59,7 @@ class PasswordDialog(tk.simpledialog.Dialog):
         return True
 
     def apply(self):
+        """
+        Store the password for later retrieval.
+        """
         self.result = self.password_entry1.get()
--- text_encryption.py
+++ text_encryption.py
@@ -6,7 +6,19 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Random import get_random_bytes
 from password_dialog import PasswordDialog
 
+
 class TextEditor:
+    """
+    A simple text editor with file encryption and decryption features.
+
+    Attributes:
+    root (tkinter.Tk): The root window of the application.
+    text_area (tkinter.Text): The text area for editing text.
+
+    Methods:
+    open_file(): Opens an encrypted file.
+    save_file(): Saves the current text to an encrypted file.
+    """
     def __init__(self, root):
         self.text_area = tk.Text(root)
         self.text_area.pack(fill="both", expand=True)
@@ -19,6 +31,15 @@ class TextEditor:
         file_menu.add_command(label="Save", command=self.save_file)
 
     def save_file(self):
+        """
+        Encrypts the current text and saves it to a file.
+
+        This method prompts the user for a file path and a password.
+        A random salt is generated and combined with the password to derive a key.
+        The current text is encrypted with this key, and the resulting ciphertext is written to the selected file,
+        preceded by the salt and the iv used for encryption.
+        If the user cancels the file dialog or the password dialog, the method returns without saving the file.
+        """
         password_dialog = PasswordDialog(root)
         password = password_dialog.result
 
@@ -45,6 +66,15 @@ class TextEditor:
         tk.messagebox.showinfo("File Saved", "File saved successfully.")
 
     def open_file(self):
+        """
+        Opens an encrypted file and displays its decrypted contents in the text area.
+
+        This method prompts the user to select a file, and then prompts for a password.
+        The selected file is read as binary and divided into salt, iv, and ciphertext parts.
+        The salt and password are used to derive a key, which is then used to decrypt the ciphertext.
+        The decrypted plaintext is displayed in the text area.
+        If the user cancels the file dialog or the password dialog, the method returns without opening the file.
+        """
         file_path = filedialog.askopenfilename(defaultextension=".enc")
         if not file_path:
             return
